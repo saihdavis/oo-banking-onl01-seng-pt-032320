f1f96daab1dac0c7f76a573c236ba47aca897fb9
Saida Davis <saihdavis@gmail.com>
2020-05-04 19:30:57 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,29 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :status, :amount
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender 
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid? 
+  end
+  
+  def execute_transaction
+   if @status == "pending" && @sender.valid?
+    @sender.balance -= @amount
+    @receiver.balance += @amount
+  end
+    @status = "complete"
+  end
+    if !sender.valid? || @sender.balance < @amount
+     @status = "rejected"
+     "Transaction rejected. Please check your account balance."
+  end
 end
+  
+
